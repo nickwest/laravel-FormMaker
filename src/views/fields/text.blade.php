b7f6c512555189c603cc5e840050549f7572cc08
Nick West <nickwest@uw.edu>
2015-07-17 13:41:30 -0700

    examples on text fields

--- src/views/fields/text.blade.php
+++ src/views/fields/text.blade.php
@@ -1,4 +1,4 @@
 <div id="field-{{{ $field->name }}}" class="field {{{ $field->type }}}">
 	{{ Form::rawLabel($field->id, $field->label.($field->is_required ? ' <em>*</em>' : '')) }}
-	{{ Form::text($field->name.($field->is_multi ? '[]' : ''), $field->value, array('id' => $field->id, 'class' => (isset($field->classes) && $field->classes != '' ? ' '.$field->classes : '' ))) }}
+	{{ Form::text($field->name.($field->is_multi ? '[]' : ''), $field->value, array('placeholder' => $field->example, 'id' => $field->id, 'class' => (isset($field->classes) && $field->classes != '' ? ' '.$field->classes : '' ))) }}
 </div>
