a032d8dae3739307713ea65d702a9e0c443fc6d5
Nick West <nickwest@uw.edu>
2015-11-04 14:52:23 -0800

    Added Yields to form.blade

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -6,7 +6,10 @@
 	@section($section)
 @endif
 
+@yield('above_form')
+
 {{ Form::open(array('url' => $Form->getProperty('url'), 'id' => $Form->getProperty('form_id'))) }}
+	@yield('form_top')
 	<fieldset>
 	@foreach($Form->getDisplayFields() as $field)
 		@if($field->type != 'subform')
@@ -16,15 +19,17 @@
 		@endif
 	@endforeach
 	</fieldset>
-	
+
 	<fieldset class="submit_button">
 		{{ Form::submit((isset($Form->submit_button) ? $Form->submit_button : 'Save'), array('name' => 'submit_button', 'class' => 'submit-green save')) }}
 		@if($Form->getAllowDelete())
 			{{ Form::submit((isset($Form->delete_button) ? $Form->delete_button : 'Delete'), array('name' => 'submit_button', 'class' => 'submit-red delete')) }}
 		@endif
 	</fieldset>
-
+	@yield('form_bottom')
 {{ Form::close() }}
+
+@yield('above_form')
 
 @if(isset($section) && $section != '')
 	@stop
