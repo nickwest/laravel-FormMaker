0e3f8803acf32eec40cd3658ac18970e3001c276
Nick West <nickwest@uw.edu>
2015-12-04 14:45:25 -0800

    Checkbox fields can now use the option array

--- src/views/fields/checkbox.blade.php
+++ src/views/fields/checkbox.blade.php
@@ -1,4 +1,14 @@
 <div id="field-{{{ $field->name }}}" class="field {{{ $field->type }}}">
-	{{ Form::checkbox($field->name, 1, (is_array($field->value) && in_array(1, $field->value) ? true : false), array('id' => $field->id, 'class' => (isset($field->classes) && $field->classes != '' ? ' '.$field->classes : '' ))) }}
-	{{ Form::rawLabel($field->id, $field->label.($field->is_required ? ' <em>*</em>' : '')) }}
+	@if(is_array($field->options) && sizeof($field->options) > 0)
+		<strong>{{ $field->label.($field->is_required ? ' <em>*</em>' : '') }}</strong>
+		@foreach($field->options as $key => $value)
+		<div>
+			{{ Form::checkbox($field->name, $key, (is_array($field->value) && in_array($key, $field->value) ? true : false), array('id' => 'cb-'.$key)) }}
+			{{ Form::rawLabel('cb-'.$key, $value) }}
+		</div>
+		@endforeach
+	@else
+		{{ Form::checkbox($field->name, 1, (is_array($field->value) && in_array(1, $field->value) ? true : false), array('id' => $field->id, 'class' => (isset($field->classes) && $field->classes != '' ? ' '.$field->classes : '' ))) }}
+		{{ Form::rawLabel($field->id, $field->label.($field->is_required ? ' <em>*</em>' : '')) }}
+	@endif
 </div>
