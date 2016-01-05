557c3031e6ba9ad890503d8b2b7e9f42276ae376
Nick West <nickwest@uw.edu>
2016-01-05 13:31:10 -0800

    Made checkbox field view obey use is_multi

--- src/views/fields/checkbox.blade.php
+++ src/views/fields/checkbox.blade.php
@@ -3,12 +3,12 @@
 		<strong>{{ $field->label.($field->is_required ? ' <em>*</em>' : '') }}</strong>
 		@foreach($field->options as $key => $value)
 		<div>
-			{{ Form::checkbox($field->name, $key, (is_array($field->value) && in_array($key, $field->value) ? true : false), array('id' => 'cb-'.$key)) }}
+			{{ Form::checkbox($field->name.($field->is_multi ? '[]' : ''), $key, (is_array($field->value) && in_array($key, $field->value) ? true : false), array('id' => 'cb-'.$key)) }}
 			{{ Form::rawLabel('cb-'.$key, $value) }}
 		</div>
 		@endforeach
 	@else
-		{{ Form::checkbox($field->name, 1, (is_array($field->value) && in_array(1, $field->value) ? true : false), array('id' => $field->id, 'class' => (isset($field->classes) && $field->classes != '' ? ' '.$field->classes : '' ))) }}
+		{{ Form::checkbox($field->name.($field->is_multi ? '[]' : ''), 1, (is_array($field->value) && in_array(1, $field->value) ? true : false), array('id' => $field->id, 'class' => (isset($field->classes) && $field->classes != '' ? ' '.$field->classes : '' ))) }}
 		{{ Form::rawLabel($field->id, $field->label.($field->is_required ? ' <em>*</em>' : '')) }}
 	@endif
 </div>
