37583a439deba925be052d475595a77d8a677e22
Marie West <westmar@uw.edu>
2016-09-02 13:21:51 -0700

    View-only form fields now use field classes.

--- src/views/fields-viewonly/checkbox.blade.php
+++ src/views/fields-viewonly/checkbox.blade.php
@@ -1,20 +1,18 @@
 <div id="field-{{{ $field->name }}}" class="field {{{ $field->type }}}">
 	@if(is_array($field->options) && sizeof($field->options) > 0)
-		<strong>{{ $field->label.($field->is_required ? ' <em>*</em>' : '') }}</strong>
-		<?php $hasDegreeOptions = false; ?>
+		{{ Form::rawLabel( $field->label,$field->label.($field->is_required ? ' <em>*</em>' : '')) }}
+		<?php $isEmpty = true; ?>
 		@foreach($field->options as $key => $value)
-		<div>
 			@if(is_array($field->value) && in_array($key, $field->value))
-				{{ Form::rawLabel('cb-'.$key, $value) }}
-				<?php $hasDegreeOptions = true; ?>
+				<span id="cb-{{{ $key }}}">{{ $value }}</span>
+				<?php $isEmpty = false; ?>
 			@endif
-		</div>
 		@endforeach
-		@if(!$hasDegreeOptions)
+		@if($isEmpty)
 			<span>{{{ 'N/A' }}}</span>
 		@endif
 	@else
 		{{ Form::rawLabel($field->id, $field->label.($field->is_required ? ' <em>*</em>' : '')) }}
-		<span>{{ $field->value ? 'True' : 'False' }}</span>
+		<span class="{{ $field->classes }}">{{ $field->value ? 'True' : 'False' }}</span>
 	@endif
 </div>
--- src/views/fields-viewonly/select.blade.php
+++ src/views/fields-viewonly/select.blade.php
@@ -1,4 +1,4 @@
 <div id="field-{{{ $field->name }}}" class="field {{{ $field->type }}}">
 	{{ Form::rawLabel($field->id, $field->label.($field->is_required ? ' <em>*</em>' : '')) }}
-	<span>{{ $field->value ? 'True' : 'False' }}</span>
+	<span class="{{ $field->classes }}">{{ $field->value }}</span>
 </div>
