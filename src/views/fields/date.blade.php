c34ffae686f0d9067c8e1124fe1ea9eef4eb15dd
Nick West <nickwest@uw.edu>
2016-10-04 09:19:51 -0700

    added date field

---
+++ src/views/fields/date.blade.php
@@ -0,0 +1,7 @@
+<div id="field-{{ $field->name.($field->multi_key != '' ? '_'.$field->multi_key : '') }}" class="field {{ $field->type }}{{ ($field->error_message ? ' error' : '') }}"{{ $field->max_length > 0 ? ' maxlength="'.$field->max_length.'"' : '' }}>
+	@if($field->label != ''){!! Form::rawLabel($field->id, $field->label.($field->label_postfix != '' ? $field->label_postfix : '').($field->is_required ? ' <em>*</em>' : '')) !!}@endif
+	{!! Form::date($field->name.($field->multi_key != '' ? '['.$field->multi_key.']' : ''), ($field->value == '' ? $field->default_value : $field->value), $field->attributes) !!}
+	@if($field->error_message)
+		<div class="error_message">{{ $field->error_message }}</div>
+	@endif
+</div>
