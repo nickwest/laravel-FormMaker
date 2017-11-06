71fce042319bf2aa1c720686fab301a0f486cba7
Nick West <nickwest@uw.edu>
2017-11-06 10:45:37 -0800

    Fixed blade::directive('formmaker_component

--- src/FormMakerServiceProvider.php
+++ src/FormMakerServiceProvider.php
@@ -23,8 +23,6 @@ class FormMakerServiceProvider extends ServiceProvider {
         $this->loadViewsFrom(__DIR__.'/views', 'form-maker');
 
         Blade::directive('formmaker_include', function($expression) {
-            // $expression = self::fixExpression($expression);
-
             if(strpos($expression, ',') !== false)
             {
                 $view = substr($expression, 0, strpos($expression, ','));
@@ -42,15 +40,26 @@ class FormMakerServiceProvider extends ServiceProvider {
             }else{
                 echo $__env->make(\'form-maker::'.$template.$remainder.', array_except(get_defined_vars(), array(\'__data\', \'__path\')))->render();
             } ?>';
-
-
-            return "<?php echo \$__env->make({$expression}, array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>";
         });
 
         Blade::directive('formmaker_component', function($expression) {
-            $expression = self::fixExpression($expression);
+            if(strpos($expression, ',') !== false)
+            {
+                $view = substr($expression, 0, strpos($expression, ','));
+                $remainder = substr($expression, strpos($expression, ','));
+            }
+            else
+            {
+                $view = $expression;
+                $remainder = '';
+            }
+            $template = substr($view, strpos($view, '::')+2);
 
-            return "<?php \$__env->startComponent({$expression}); ?>";
+            return '<?php if(View::exists('.$view.')){
+                $__env->startComponent('.$expression.');
+            }else{
+                $__env->startComponent(\'form-maker::'.$template.$remainder.');
+            } ?>';
         });
 
     }
@@ -73,35 +82,6 @@ class FormMakerServiceProvider extends ServiceProvider {
     public function provides()
     {
         return [];
-    }
-
-    /**
-     * Adjust a blade directive's expression for supporting fallback template namespaces
-     *
-     * @param string $expression
-     * @return string
-     */
-    static public function fixExpression(string $expression) : string
-    {
-        if(strpos($expression, ',') !== false)
-        {
-            $view = str_replace('\'', '', substr($expression, 0, strpos($expression, ',')-1));
-            $remainder = substr($expression, strpos($expression, ','));
-        }
-        else
-        {
-            $view = str_replace('\'', '', $expression);
-            $remainder = '';
-        }
-        $template = substr($view, strpos($view, '::')+2);
-
-        if(!View::exists($view)){
-            $view = 'form-maker::'.$template;
-        }
-
-        $expression = '\''.$view.'\''.$remainder;
-
-        return $expression;
     }
 
 }
