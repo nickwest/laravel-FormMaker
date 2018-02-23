f632e435ad95b4f39b4deaa9a69a9f5f8ae7c54b
Nick West <nickwest@uw.edu>
2018-02-23 13:48:56 -0800

    Tweaks to content-based fields

--- src/views/components/field.blade.php
+++ src/views/components/field.blade.php
@@ -1,4 +1,4 @@
-<div id="field-{{ $Field->attributes->name.($Field->multi_key != '' ? '_'.$Field->multi_key : '') }}" class="field {{ ($Field->attributes->required ? 'required ' : '') }}{{ $Field->container_class }}">
+<div id="field-{{ $Field->attributes->name.($Field->multi_key != '' ? '_'.$Field->multi_key : '') }}" class="type-{{ $Field->attributes->type }} field {{ ($Field->attributes->required ? 'required ' : '') }}{{ $Field->container_class }}">
 
 	{!! $field_markup !!}
 
